e12540a2ff623a7091d77a8bc1f3fdc6b09dd808
jasperaeschlimann <jasper.aeschlimann@outlook.com>
2024-11-17 20:27:48 +0000

    Updated comments on planet_constants

--- simulation/planet_constants.py
+++ simulation/planet_constants.py
@@ -1,11 +1,11 @@
 # Gravitational Constant
 G_CONSTANT = 6.67430e-11  # in m^3 kg^-1 s^-2
 
-# Earth
+# Earth parameters
 EARTH_MASS = 5.972e24  # in kilograms
 EARTH_RADIUS = 6.371e6  # in meters
 
-# Moon 
+# Moon parameters
 MOON_MASS = 7.347e22  # in kilograms
 MOON_RADIUS = 1.737e6  # in meters
 MOON_DISTANCE = 3.85e8  # Distance from Earth in meters
